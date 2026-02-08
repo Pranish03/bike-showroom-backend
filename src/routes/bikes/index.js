aa3d9211557b20e05df376fbc4c8219f0ba852e6
Pranish Chaulagain <cpranishabc163@gmail.com>
2026-02-08 19:56:41 +0545

    Add create and update validation

--- src/routes/bikes/index.js
+++ src/routes/bikes/index.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
 import { requireAuth } from "../../middlewares/requireAuth.js";
+import { requireValidation } from "../../middlewares/requireValidation.js";
 import { Bike } from "./models.js";
+import {
+  createBikeValidationSchema,
+  updateBikeValidationSchema,
+} from "./validation.js";
 
 const bikeRouter = Router();
 
@@ -10,17 +15,24 @@ const bikeRouter = Router();
  * @API    POST /bike/
  */
 
-bikeRouter.post("/", requireAuth({ isAdmin: true }), async (req, res) => {
-  try {
-    const data = req.body;
-    const bike = await Bike.create(data);
+bikeRouter.post(
+  "/",
+  requireAuth({ isAdmin: true }),
+  requireValidation(createBikeValidationSchema),
+  async (req, res) => {
+    try {
+      const data = req.body;
+      const bike = await Bike.create(data);
 
-    return res.status(200).json({ Message: "Bike created successfully", bike });
-  } catch (error) {
-    console.log(error);
-    return res.status(500);
-  }
-});
+      return res
+        .status(200)
+        .json({ Message: "Bike created successfully", bike });
+    } catch (error) {
+      console.log(error);
+      return res.status(500);
+    }
+  },
+);
 
 /**
  * @Desc   Get all bikes
@@ -63,21 +75,28 @@ bikeRouter.get("/:id", async (req, res) => {
  * @API    PUT /bike/:id
  */
 
-bikeRouter.put("/:id", requireAuth({ isAdmin: true }), async (req, res) => {
-  try {
-    const bikeId = req.params.id;
-    const data = req.body;
+bikeRouter.put(
+  "/:id",
+  requireAuth({ isAdmin: true }),
+  requireValidation(updateBikeValidationSchema),
+  async (req, res) => {
+    try {
+      const bikeId = req.params.id;
+      const data = req.body;
 
-    const bike = await Bike.findByIdAndUpdate(bikeId, data);
+      const bike = await Bike.findByIdAndUpdate(bikeId, data, { new: true });
 
-    if (!bike) return res.status(404).json({ message: "Bike not found" });
+      if (!bike) return res.status(404).json({ message: "Bike not found" });
 
-    return res.status(200).json({ message: "Bike updated successfully", bike });
-  } catch (error) {
-    console.log(error);
-    return res.status(500);
-  }
-});
+      return res
+        .status(200)
+        .json({ message: "Bike updated successfully", bike });
+    } catch (error) {
+      console.log(error);
+      return res.status(500);
+    }
+  },
+);
 
 /**
  * @Desc   Delete bike by id
--- src/routes/bikes/validation.js
+++ src/routes/bikes/validation.js
@@ -1,1 +1,12 @@
 // To do validation
+import { z } from "zod";
+
+export const createBikeValidationSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  description: z.string().min(1, "Description is required"),
+  price: z.string().min(1, "Price is required"),
+  companyName: z.string().min(1, "Company name is required"),
+  details: z.string().optional,
+});
+
+export const updateBikeValidationSchema = createBikeValidationSchema.partial();
