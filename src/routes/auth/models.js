92f1172b67415df735bfe45eb5d0765eec66c274
Pranish Chaulagain <cpranishabc163@gmail.com>
2026-02-09 09:20:23 +0545

    Finish backend

--- src/routes/auth/models.js
+++ src/routes/auth/models.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     username: { type: String },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    isAdmin: Boolean,
+    isAdmin: { type: Boolean, default: false },
   },
   {
     timestamps: true,
