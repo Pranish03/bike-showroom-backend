444468dc846cf54a613cc94ba341d17761790c97
Pranish Chaulagain <cpranishabc163@gmail.com>
2026-02-28 20:41:45 +0545

    Update validation

--- src/routes/contact/validation.js
+++ src/routes/contact/validation.js
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const contactValidationSchema = z.object({
-  name: z.string().min(1, "Name is required"),
-  email: z.email(),
-  subject: z.string().min(1, "Subject is required"),
-  message: z.string().min(5, "Message must be at least 5 characters"),
+  name: z.string().min(3, "Name must have atleast 3 characters").trim(),
+  email: z.email("Invalid email address").trim(),
+  subject: z.string().min(1, "Subject is required").trim(),
+  message: z.string().min(5, "Message must have atleast 5 characters").trim(),
 });
